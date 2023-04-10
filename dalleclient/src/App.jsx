4dc1bd194fec8af53d6f75e059130ef71cb36b1b
arulkumararul <arulkumararul2000@gmail.com>
2023-04-10 14:04:34 +0530

    dalle and chatgpt connect

---
+++ dalleclient/src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import { logo } from "./assets";
+import { Home, CreatePost } from "./page";
+
+const App = () => (
+  <BrowserRouter>
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <Link to="/">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+      </Link>
+
+      <Link
+        to=""
+        className="font-inter flex flex-column font-medium bg-[#f34e64] text-white px-4 py-2 rounded-md mr-2 ml-auto text-xs md:text-lg"
+      >
+        <img src="../favicon.ico" alt="Bot" className="hidden md:block w-5 mr-2"/>
+        ChatGPT
+      </Link>
+
+      <Link
+        to="/create-post"
+        className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md text-xs md:text-lg"
+      >
+        Create
+      </Link>
+    </header>
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </main>
+  </BrowserRouter>
+);
+
+export default App;
