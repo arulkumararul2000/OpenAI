f03cc4b621694a1d61d54ca40f63bc95a950ec7e
arulkumararul <arulkumararul2000@gmail.com>
2023-04-10 15:54:11 +0530

    link

--- dalleclient/src/App.jsx
+++ dalleclient/src/App.jsx
@@ -11,14 +11,14 @@ const App = () => (
         <img src={logo} alt="logo" className="w-28 object-contain" />
       </Link>
 
-      <Link className="font-inter flex flex-column font-medium bg-[#f34e64] text-white px-4 py-2 rounded-md mr-2 ml-auto text-xs md:text-lg">
+      <link href="https://chatgptdalle.netlify.app/" className="font-inter flex flex-column font-medium bg-[#f34e64] text-white px-4 py-2 rounded-md mr-2 ml-auto text-xs md:text-lg">
         <img
           src="../favicon.ico"
           alt="Bot"
           className="hidden md:block w-5 mr-2"
         />
-        <a href="https://chatgptdalle.netlify.app/">ChatGPT</a>
-      </Link>
+        ChatGPT
+      </link>
 
       <Link
         to="/create-post"
